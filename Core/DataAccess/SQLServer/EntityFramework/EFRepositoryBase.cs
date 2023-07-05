329268cd86dc5ce2326b5e4aaabcf85a74da26a2
axmed97 <axmed.dev.13@gmail.com>
2023-07-05 14:31:39 +0400

    Add project files.

---
+++ Core/DataAccess/SQLServer/EntityFramework/EFRepositoryBase.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess.SQLServer.EntityFramework
+{
+    public class EFRepositoryBase<TEntity, TContext> : IRepositoryBase<TEntity>
+        where TEntity : class
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            // AppDbContext context = new();
+            // contex.Add(entity)
+            // using directive
+            using TContext context = new();
+            var addEntity = context.Entry(entity);
+            addEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>>? expression = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
